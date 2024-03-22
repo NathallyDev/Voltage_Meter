3b4395d2b97ab5246c97cdb4f7cd511ad374d399
Náthally Lima Arruda <nathallylym@gmail.com>
2024-03-22 11:37:21 -0300

    Codigo Okay

--- Codigo_fonte/plotagem.py
+++ Codigo_fonte/plotagem.py
@@ -9,26 +9,22 @@
 #
 
 import serial
-import matplotlib.pyplot as plt  # Adicionando importação do módulo matplotlib.pyplot
+import matplotlib.pyplot as plt
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 import time
 from serial.tools.list_ports import comports
 from io import BytesIO
-import tempfile
 import os
-import pdfkit
-import numpy as np
+from PIL import Image  
 
 def auto_select_serial_port():
-    # Função para selecionar automaticamente a porta serial
-    ports = comports()
+    # Função para selecionar a porta serial
 
+    ports = comports()
     for port, desc, hwid in sorted(ports):
         pass
-
     time.sleep(2)
-
     if ports:
         selected_port = ports[0].device
         return selected_port
@@ -36,30 +32,29 @@ def auto_select_serial_port():
         return None
 
 def read_and_plot_data(modelo, tempo_total_segundos, com_port):
-    # Função para ler e plotar dados
+    # Função para ler e plotar gráficos
+
     tempo_total = []
     tensao_total = []
     intervalo_leitura = 1
 
-    print("Iniciando coleta de dados...")
-
+    print("Coletando dados...")
     ser = None
+
     try:
         ser = serial.Serial(com_port, 9600, timeout=1)
-
         start_time = time.time()
-
         while time.time() - start_time < tempo_total_segundos:
             data = ser.readline().decode('utf-8').strip()
             if data:
                 tempo_atual, tensao_atual = map(float, data.split(','))
                 tempo_total.append(tempo_atual)
                 tensao_total.append(tensao_atual)
-
             time.sleep(intervalo_leitura)
 
     except KeyboardInterrupt:
         pass
+
     except Exception as e:
         print(f"Erro ao ler da porta serial: {e}")
 
@@ -71,26 +66,24 @@ def read_and_plot_data(modelo, tempo_total_segundos, com_port):
     if not tempo_total or not tensao_total:
         print("Nenhum dado lido.")
         return None, None
-
+    
     return tempo_total, tensao_total
 
 def plot_graph(tempo, tensao, titulo, modelo, t_graph):
-    # Definir o tamanho da figura para garantir clareza
+# definir o tamanho da figura para garantir clareza
+        
     plt.figure(figsize=(10, 6))
-    tempo_minutos = [t / 60000 for t in tempo]  # Converter milissegundos para minutos
+    tempo_minutos = [t / 60000 for t in tempo]
 
-    # Plotar o gráfico
+# Definindo parametros do gráfico
     plt.plot(tempo_minutos, tensao)
     plt.xlabel('Tempo (min)')
     plt.ylabel('Tensão')
     plt.title(titulo)
     plt.ylim(0, 30)
-    plt.xlim(0, t_graph + 1)  # Adiciona uma unidade extra ao limite máximo do eixo x
-
-    # Adicionar grade ao gráfico
+    plt.xlim(0, t_graph + 1)
     plt.grid(True)
 
-    # Salvar o gráfico em um buffer de bytes
     buffer = BytesIO()
     plt.savefig(buffer, format='png')
     buffer.seek(0)
@@ -99,13 +92,15 @@ def plot_graph(tempo, tensao, titulo, modelo, t_graph):
     return buffer
 
 def run_experiment():
-    # Função para executar o experimento
-    com_port = auto_select_serial_port()
+# Função para iniciar o experimento e realizar a coleta de dados
 
+    com_port = auto_select_serial_port()
     if com_port is None:
         print("Não foi possível selecionar uma porta serial.")
-        return
 
+        return
+    
+# Coleta de dados para o PDF
     nome = input("Nome do equipamento: ")
     fabricante = input("Nome do fabricante: ")
     p_n = input("P/N: ")
@@ -113,28 +108,43 @@ def run_experiment():
     modelo = input("Modelo do equipamento: ")
     t_graph = int(input("Informe o tempo para a realização do teste (em minutos): "))
 
+# Reorganizando o tempo para a plotagem do gráfico
     tempo_total_segundos = t_graph * 60
     tempo_total, tensao_total = read_and_plot_data(modelo, tempo_total_segundos, com_port)
     titulo = f"Tensão ao Longo do Tempo ({modelo})"
-    
+
+# Continuação da coleta de dados do PDF
     ad = input("Houve AD: ")
     num_ad = input("Informe o número da AD: ")
     observacao = input("O.B.S: ")
-
     graph_buffer = plot_graph(tempo_total, tensao_total, titulo, modelo, t_graph)
+    generate_pdf(nome, fabricante, p_n, s_n, modelo, titulo, tempo_total, tensao_total, observacao, graph_buffer, ad, num_ad, t_graph)
 
-    generate_pdf(nome, fabricante, p_n, s_n, modelo, titulo, tempo_total, tensao_total, observacao, graph_buffer, ad, num_ad)
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from PIL import Image
+import os
+import time
 
-def generate_pdf(nome, fabricante, p_n, s_n, modelo, titulo, tempo_total, tensao_total, observacao, graph_buffer, ad, num_ad):
+def generate_pdf(nome, fabricante, p_n, s_n, modelo, titulo, tempo_total, tensao_total, observacao, graph_buffer, ad, num_ad, t_graph):
     pdf_filename = f"{p_n}_{s_n}_{time.strftime('%Y%m%d_%H%M%S')}.pdf"
-
     pdf = canvas.Canvas(pdf_filename, pagesize=letter)
     pdf.setFont("Helvetica", 12)
+    pdf.drawString(72, 750, f"Equipamento: {nome}       Fabricante: {fabricante}      Modelo: {modelo} ")
+    pdf.drawString(72, 735, f"P/N: {p_n}    S/N: {s_n}    AD: {ad}       AD NUMBER: {num_ad}")
 
-    pdf.drawString(72, 750, f"Equipamento: {nome}       Fabricante: {fabricante}")
-    pdf.drawString(72, 735, f"Modelo: {modelo}       P/N: {p_n}    S/N: {s_n}    AD: {ad}       AD NUMBER: {num_ad}")
+    # Define o caminho da imagem com base no P/N
+    pn_to_image = {
+        "501-1228-01": r"C:\xampp\htdocs\GitHub\Voltage_Meter\Codigo_fonte\Imagem_1.png",
+        "501-1228-03": r"C:\xampp\htdocs\GitHub\Voltage_Meter\Codigo_fonte\Imagem_1.png",
+        "501-1228-05": r"C:\xampp\htdocs\GitHub\Voltage_Meter\Codigo_fonte\Imagem_1.png",
+        "501-1228-02": r"C:\xampp\htdocs\GitHub\Voltage_Meter\Codigo_fonte\Imagem_2.png",
+        "501-1228-04": r"C:\xampp\htdocs\GitHub\Voltage_Meter\Codigo_fonte\Imagem_2.png",
+        "501-1228-06": r"C:\xampp\htdocs\GitHub\Voltage_Meter\Codigo_fonte\Imagem_2.png",
+        "501-1228-07": r"C:\xampp\htdocs\GitHub\Voltage_Meter\Codigo_fonte\Imagem_2.png"
+    }
 
-    # Salvar o buffer de bytes em um arquivo temporário
+    # Salva o buffer de bytes em um arquivo temporário
     temp_filename = f"{p_n}_{s_n}_{time.strftime('%Y%m%d_%H%M%S')}.png"
     with open(temp_filename, 'wb') as f:
         f.write(graph_buffer.getvalue())
@@ -142,10 +152,24 @@ def generate_pdf(nome, fabricante, p_n, s_n, modelo, titulo, tempo_total, tensao
     # Adiciona a imagem do gráfico ao PDF
     pdf.drawImage(temp_filename, 100, 400, width=400, height=300)
 
-    # Escreve as observações abaixo do gráfico
-    pdf.drawString(72, 350, f"Observações: {observacao}")
+    # Verifica se o P/N está no dicionário
+    if p_n in pn_to_image:
+        image_name = pn_to_image[p_n]
+        # Verifica se o arquivo de imagem está na mesma pasta do código
 
-    # Salva o PDF
+        img_height = 0  # Define um valor padrão
+        if os.path.exists(image_name):
+            img = Image.open(image_name)
+            img_width, img_height = img.size  # Atualiza o valor de img_height se a imagem existir
+            pdf.drawImage(image_name, 100, 50, width=400, height=300)
+            pdf.drawString(72, 50 + img_height + 30, f"Observações: {observacao}")
+        else:
+            print(f"Imagem {image_name} não encontrada na pasta do código.")
+            pdf.drawString(72, 50 + img_height + 30, f"Observações: {observacao}")
+    else:
+        print(f"P/N {p_n} não encontrado no dicionário de imagens.")
+        pdf.drawString(72, 400, f"Observações: {observacao}")
+
     pdf.save()
     print("PDF Gerado.")
 
@@ -153,3 +177,12 @@ def generate_pdf(nome, fabricante, p_n, s_n, modelo, titulo, tempo_total, tensao
     os.unlink(temp_filename)
 
 run_experiment()
+
+
+run_experiment()
+
+
+        
+
+            
+       
